fdd4b0d2da6b9ceb77ba7c91f13841644bd418fb
SuganyaDuraisamy <dsuganya@velalarengg.ac.in>
2025-07-07 13:01:42 +0530

    Removed a Comment

--- Palindraome.py
+++ Palindraome.py
@@ -1,4 +1,3 @@
-# Function to check if a string is a palindrome
 def is_palindrome(s):
     s = ''.join(c.lower() for c in s if c.isalnum())  # Normalize: remove non-alphanumeric, lowercase
     return s == s[::-1]
